4830cde5a6462339e7ce21bf882febbb1ff6d97d
renisv <vukajrenis@gmail.com>
2025-11-25 10:53:06 +0100

    update

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-This module contains a function that returns the floor of a float.
+This module contains a function that returns the floor of a float number.
 """
 
 import math
@@ -8,7 +8,7 @@ import math
 
 def floor(n: float) -> int:
     """
-    Return the floor of the float n.
+    Return the floor of the float number n.
 
     Args:
         n (float): The number to floor.
