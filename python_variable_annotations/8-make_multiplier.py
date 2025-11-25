f51b69ec2abf7944f91c2f57e70fcff2fa4be7df
renisv <vukajrenis@gmail.com>
2025-11-25 10:58:29 +0100

    update

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-This module contains a function that returns a multiplier function.
+Multiplier function
 """
 
 from typing import Callable
@@ -8,13 +8,7 @@ from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
-    Return a function that multiplies a float by the given multiplier.
-
-    Args:
-        multiplier (float): The multiplier value.
-
-    Returns:
-        Callable[[float], float]: A function that multiplies its input by multiplier.
+    Multiplier function
     """
     def multiplier_func(n: float) -> float:
         return n * multiplier
--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-This module contains a function that returns a list of tuples of iterable elements and their lengths.
+Element lengths
 """
 
 from typing import Iterable, Sequence, List, Tuple
@@ -8,12 +8,6 @@ from typing import Iterable, Sequence, List, Tuple
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
-    Return a list of tuples where each tuple contains an element from lst and its length.
-
-    Args:
-        lst (Iterable[Sequence]): An iterable of sequence elements.
-
-    Returns:
-        List[Tuple[Sequence, int]]: List of tuples with (element, length).
+    Element lengths
     """
     return [(i, len(i)) for i in lst]
