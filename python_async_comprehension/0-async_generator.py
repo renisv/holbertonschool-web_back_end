81745a9856b8f58e192ae237bf641167d0c04419
renisv <vukajrenis@gmail.com>
2025-11-26 10:07:15 +0100

    update

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Async Generator module"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    Coroutine that loops 10 times, asynchronously waits 1 second,
+    then yields a random number between 0 and 10.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
