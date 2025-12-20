dfd155edbad2bfce8e8bd8e853d6f3aa0b574661
renisv <vukajrenis@gmail.com>
2025-12-20 11:33:05 +0100

    update

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,10 +1,9 @@
-import uploadPhoto from './utils.js';
-import createUser from './utils.js';
+import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,13 +1,12 @@
 export default function guardrail(mathFunction) {
   const queue = [];
-  
   try {
     const result = mathFunction();
     queue.push(result);
   } catch (error) {
-    queue.push(error.message);
+    queue.push(error.toString());
+  } finally {
+    queue.push('Guardrail was processed');
   }
-  
-  queue.push('Guardrail was processed');
   return queue;
 }
